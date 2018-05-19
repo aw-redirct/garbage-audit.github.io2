2dd3ab1feeacfc45f30afc58495879185e3acf57
YasserNoori <38815702+YasserNoori@users.noreply.github.com>
2018-05-19 16:20:16 -0400

    Update jquery.style.switcher.js

--- js/jquery.style.switcher.js
+++ js/jquery.style.switcher.js
@@ -5,7 +5,7 @@ jQuery Style Switcher
 
 The MIT License (MIT)
 
-Copyright (c) 2014 Cameron Manavian
+Copyright (c) 2018 Yasser Noori
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
