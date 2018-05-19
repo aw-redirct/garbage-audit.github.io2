fd1af0919f65ed2960db2751bcec21a3193d9b80
YasserNoori <38815702+YasserNoori@users.noreply.github.com>
2018-05-19 16:33:16 -0400

    Update google_map.js

--- js/google_map.js
+++ js/google_map.js
@@ -11,7 +11,7 @@ function init() {
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 12,
+        zoom: 18,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
